570dd65f7f2a86c27ebe0b115a5d82422a242ac2
Sourav <souravghosh1500@gmail.com>
2021-10-11 21:09:28 +0530

    small changes

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var port=8000
 app.get('/',(req,res)=>{
     return res.send("App is running")
 })
-app.listen(port, () => {
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen(port,host, () => {
     console.log(`Server running on Port - ${port}`);
     
   });
